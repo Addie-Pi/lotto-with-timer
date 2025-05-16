b6e08fa2f9e691b92c2bf0753a55cfd6c319cfa2
Addie <addiepi306@gmail.com>
2025-05-16 20:32:35 +0800

    add counting word

--- src/components/CountdownTimer/CountdownTimer.js
+++ src/components/CountdownTimer/CountdownTimer.js
@@ -9,66 +9,70 @@ import './CountdownTimer.css'
 
 
 const CountdownTimer = () => {
-    const [inputMinutes, setInputMinutes] = useState('')
-    const [countdownMinutes, setCountdownMinutes] = useState();
-    const [textMinutes, setTextMinutes] = useState('00');
-    const [textSeconds, setTextSeconds] = useState('00');
+  const [inputMinutes, setInputMinutes] = useState('')
+  const [countdownMinutes, setCountdownMinutes] = useState();
+  const [textMinutes, setTextMinutes] = useState('00');
+  const [textSeconds, setTextSeconds] = useState('00');
+  const [isRunning, setIsRunning] = useState(false);
 
-    const dispatch = useDispatch();
-   
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-      if (countdownMinutes >= 0) {
-        const toCountdown = setInterval(() => {
-          caluculateCountdownTime(countdownMinutes)
-        }, 1000)
 
-        return () => {
-          clearInterval(toCountdown)
-        }
-      } else {
-        dispatch(changeCountdownOverstatus());
-        dispatch(selectWinner());
+  useEffect(() => {
+    if (!isRunning) return
+
+    if (countdownMinutes > 0) {
+      const toCountdown = setInterval(() => {
+        caluculateCountdownTime(countdownMinutes)
+      }, 1000)
+
+      return () => {
+        clearInterval(toCountdown)
       }
-    }, [countdownMinutes, dispatch])
-
-
-    const caluculateCountdownTime = (totalCountdownTimeInSecs) => { 
-      const calculatSecs = totalCountdownTimeInSecs % 60;
-      const calculatMins = Math.floor(totalCountdownTimeInSecs / 60);
-      console.log('calculatSecs', calculatSecs);
-      console.log('calculatMins', calculatMins)
-
-      if (totalCountdownTimeInSecs >= 0) {
-        
-        totalCountdownTimeInSecs --;
-        const calculatSecs = totalCountdownTimeInSecs % 60
-        const calculatMins = Math.floor(totalCountdownTimeInSecs / 60)
-
-        setCountdownMinutes(totalCountdownTimeInSecs);
-        setTextMinutes(('0' + calculatMins).slice(-2));
-        setTextSeconds(('0' + calculatSecs).slice(-2));
-      }
+    } else {
+      dispatch(changeCountdownOverstatus());
+      dispatch(selectWinner());
     }
+  }, [countdownMinutes, isRunning, dispatch])
 
-    const getInputMinutes = (event) => {
-      setInputMinutes(event.target.value);
+
+  const caluculateCountdownTime = (totalCountdownTimeInSecs) => {
+    let calculatSecs = totalCountdownTimeInSecs % 60;
+    let calculatMins = Math.floor(totalCountdownTimeInSecs / 60);
+
+
+    if (totalCountdownTimeInSecs >= 0) {
+      totalCountdownTimeInSecs--;
+      calculatSecs = totalCountdownTimeInSecs % 60
+      calculatMins = Math.floor(totalCountdownTimeInSecs / 60)
+
+      setCountdownMinutes(totalCountdownTimeInSecs);
+      setTextMinutes(('0' + calculatMins).slice(-2));
+      setTextSeconds(('0' + calculatSecs).slice(-2));
     }
+  }
 
-    const submit = (event) => {
-      event.preventDefault();
+  const getInputMinutes = (event) => {
+    setInputMinutes(event.target.value);
+  }
 
-      caluculateCountdownTime(inputMinutes * 60);
+  const submit = (event) => {
+    event.preventDefault();
 
-      // setCountdownMinutes(inputMinutes * 60-1);
-     
-      setInputMinutes('');
-    }
+    caluculateCountdownTime(inputMinutes * 60);
+    setIsRunning(true);
+    setInputMinutes('');
+  }
 
 
-    
-    return (
-      <div className="countdown_container">
+
+  return (
+    <div className="countdown_container">
+      {isRunning ? (
+        <div className="countdown_status">
+          <span>抽獎中</span>
+        </div>
+      ) : (
         <form onSubmit={submit}>
           <div className="countdown_content">
             <div className="countdown_input">
@@ -90,14 +94,15 @@ const CountdownTimer = () => {
             <button type="submit">開始</button>
           </div>
         </form>
+      )}
 
-        <div className='countdown_result'>
-          <span>
-            {textMinutes} : {textSeconds}
-          </span>
-        </div>
+      <div className='countdown_result'>
+        <span>
+          {textMinutes} : {textSeconds}
+        </span>
       </div>
-    )
+    </div>
+  )
 }
 
 export default CountdownTimer
